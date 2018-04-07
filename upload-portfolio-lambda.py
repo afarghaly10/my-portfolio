2bb631fb5a25fd7d057a79ed6adee11575e45cbf
fekret.cloud <sami.farghaly@gmail.com>
2018-04-07 14:38:32 -0400

    upload-portfolio-lambda

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,20 @@
+import boto3
+import io
+import zipfile
+import StringIO
+import mimetypes
+from io import StringIO
+
+s3 = boto3.resource('s3')
+
+portfolio_bucket = s3.Bucket('afarghaly.com')
+build_bucket = s3.Bucket('portfoliobuild.afarghaly.com')
+
+portfolio_zip = io.BytesIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj= myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
