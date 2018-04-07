9725a296e4bec344c890b7bcaa3be8883be0dd3c
fekret.cloud <sami.farghaly@gmail.com>
2018-04-07 16:59:19 -0400

    update portfolio deployment function for lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,20 +1,25 @@
 import boto3
 import io
 import zipfile
-import StringIO
 import mimetypes
 from io import StringIO
 
-s3 = boto3.resource('s3')
 
-portfolio_bucket = s3.Bucket('afarghaly.com')
-build_bucket = s3.Bucket('portfoliobuild.afarghaly.com')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
 
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_bucket = s3.Bucket('afarghaly.com')
+    build_bucket = s3.Bucket('portfoliobuild.afarghaly.com')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj= myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(
+                obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    print('Job Done!')
+    return 'Hello from Lambda'
